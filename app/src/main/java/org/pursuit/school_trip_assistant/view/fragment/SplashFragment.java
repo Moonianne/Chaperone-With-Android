179457ca1df6919e7add53a975ec06e6fa2f3827
Geo Jimenez <ger.jimenez1@gmail.com>
2019-03-28 15:35:34 -0400

    create SplashFragment class and xml

---
+++ app/src/main/java/org/pursuit/school_trip_assistant/view/fragment/SplashFragment.java
@@ -0,0 +1,45 @@
+package org.pursuit.school_trip_assistant.view.fragment;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.pursuit.school_trip_assistant.R;
+import org.pursuit.school_trip_assistant.view.OnFragmentInteractionListener;
+
+public final class SplashFragment extends Fragment {
+    private OnFragmentInteractionListener onFragmentInteractionListener;
+
+    public SplashFragment() {
+    }
+
+    public static SplashFragment newInstance() {
+        return new SplashFragment();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            onFragmentInteractionListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException("Host Activity Most Implement OnFragmentInteractionListener.");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onDetach() {
+        onFragmentInteractionListener = null;
+        super.onDetach();
+    }
+}
