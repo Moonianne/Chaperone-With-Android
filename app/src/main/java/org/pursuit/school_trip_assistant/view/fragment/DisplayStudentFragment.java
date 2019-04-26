0e9b7e41872f27d51175f7a5275975274ad9cfe9
Geo Jimenez <ger.jimenez1@gmail.com>
2019-04-26 11:12:58 -0400

    WIP add Disposable

--- app/src/main/java/org/pursuit/school_trip_assistant/view/fragment/DisplayStudentFragment.java
+++ app/src/main/java/org/pursuit/school_trip_assistant/view/fragment/DisplayStudentFragment.java
@@ -19,49 +19,51 @@ import com.squareup.picasso.Picasso;
 import org.pursuit.school_trip_assistant.R;
 import org.pursuit.school_trip_assistant.view.OnFragmentInteractionListener;
 
+import io.reactivex.disposables.Disposable;
+
 public final class DisplayStudentFragment extends Fragment {
-  private static final String ID_KEY = "Display.ID";
+    private static final String ID_KEY = "Display.ID";
 
-  private OnFragmentInteractionListener onFragmentInteractionListener;
+    private OnFragmentInteractionListener onFragmentInteractionListener;
 
-  public static DisplayStudentFragment newInstance(int iD) {
-    DisplayStudentFragment displayStudentFragment = new DisplayStudentFragment();
-    Bundle args = new Bundle();
-    args.putInt(ID_KEY, iD);
-    displayStudentFragment.setArguments(args);
-    return displayStudentFragment;
-  }
-
-  @Override
-  public void onAttach(Context context) {
-    super.onAttach(context);
-    if (context instanceof OnFragmentInteractionListener) {
-      onFragmentInteractionListener = (OnFragmentInteractionListener) context;
-    } else {
-      throw new RuntimeException("Must Implement OnFragmentInteractionListener in Host Activity.");
+    public static DisplayStudentFragment newInstance(int iD) {
+        DisplayStudentFragment displayStudentFragment = new DisplayStudentFragment();
+        Bundle args = new Bundle();
+        args.putInt(ID_KEY, iD);
+        displayStudentFragment.setArguments(args);
+        return displayStudentFragment;
     }
-  }
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_display_student, container, false);
-  }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            onFragmentInteractionListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException("Must Implement OnFragmentInteractionListener in Host Activity.");
+        }
+    }
 
-  @Override
-  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_display_student, container, false);
+    }
 
-    int iD = getArguments().getInt(ID_KEY);
-    MaterialButton materialButton = view.findViewById(R.id.edit_button);
-    RxView.clicks(materialButton)
-      .subscribe(click -> onFragmentInteractionListener.editStudent(iD));
-    view.<TextView>findViewById(R.id.display_text_name).setText(getString(R.string.student_name,
-      onFragmentInteractionListener.getStudentFullName(iD)));
-    view.<TextView>findViewById(R.id.display_e_phone).setText(getString(R.string.emergency_contact,
-      onFragmentInteractionListener.getEmergencyContact(iD)));
-    Picasso.get()
-      .load(onFragmentInteractionListener.getStudentImage(iD))
-      .into(view.<ImageView>findViewById(R.id.display_image));
-  }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        int iD = getArguments().getInt(ID_KEY);
+        MaterialButton materialButton = view.findViewById(R.id.edit_button);
+        Disposable disposable = RxView.clicks(materialButton)
+          .subscribe(click -> onFragmentInteractionListener.editStudent(iD));
+        view.<TextView>findViewById(R.id.display_text_name).setText(getString(R.string.student_name,
+          onFragmentInteractionListener.getStudentFullName(iD)));
+        view.<TextView>findViewById(R.id.display_e_phone).setText(getString(R.string.emergency_contact,
+          onFragmentInteractionListener.getEmergencyContact(iD)));
+        Picasso.get()
+          .load(onFragmentInteractionListener.getStudentImage(iD))
+          .into(view.<ImageView>findViewById(R.id.display_image));
+    }
 }
